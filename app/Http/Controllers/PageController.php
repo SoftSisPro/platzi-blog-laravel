fc0f8cd9dc2c54508e223b2e3c1955a342ed4af7
SoftSisPro <softsispro@gmail.com>
2024-09-11 23:10:12 -0500

    listamos los post en la pagina principal

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,20 +9,9 @@ class PageController extends Controller
 {
     public function home()
     {
-        return view("home");
-    }
-
-    public function blog()
-    {
-        //$posts = Post::all(); //Obtener todos los post
-        //$posts = Post::get(); //Obtener todos los post
-        //$posts = Post::orderBy('id', 'DESC')->get(); //Obtener todos los post
-        
         $posts = Post::latest()->paginate(); //Obtener todos los post
 
-
-        //dd($posts); //Imprimir en pantalla
-        return view('blog', ['posts'=>$posts]);
+        return view("home", ['posts'=>$posts]);
     }
 
     public function post(Post $post)
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,7 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(PageController::class)->group(function () {
-    Route::get('/',            'home')->name('home');
-    Route::get('/blog',        'blog')->name('blog');
+    Route::get('/',                 'home')->name('home');
     Route::get('/blog/{post:slug}', 'post')->name('post');
 });
 
