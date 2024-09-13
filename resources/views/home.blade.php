5107dc14e2740785dc905ddd5a09ca359bd4c705
SoftSisPro <softsispro@gmail.com>
2024-09-12 20:40:32 -0500

    Sesion de ver el post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,12 +10,12 @@
 </div>
 
 <div class="px-4">
-    <h1 class="text-2xl mb-8 text-gray-900">Mis Publicaciones</h1>
+    <h1 class="text-2xl mb-8 text-blue-900">Mis Publicaciones</h1>
     <div class="grid grid-cols-1 gap-4 mb-4">
         @foreach ($posts as $post)
-            <a href="" class="rounded-lg bg-gray-100 px-6 py-4">
+            <a href="{{ route('post', $post->slug) }}" class="rounded-lg bg-gray-100 px-6 py-4">
                 <p class="text-xs flex items-center gap-2">
-                    <span class="uppercase text-gray-700 bg-gray-200 px-2 py-1 rounded-full">• Publicación</span>
+                    <span class="uppercase text-gray-700 bg-gray-200 px-3 py-1 rounded-full">Publicación</span>
                     <span>{{ $post->created_at->format('d/m/Y') }}</span>
 
                 </p>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,15 @@
 
 @section('content')
 
-    <h1>{{ $post->title }}</h1>
-    <p>{{ $post->body }}</p>
+<div class="max-w-3xl mx-auto">
+    <div class="flex justify-between items-center border-b border-gray-200 pb-3">
+        <a href="{{ route('home') }}" class="text-blue-700">Regresar</a>
+        <span class="bg-gray-200 text-gray-500 text-xs rounded-lg px-2 py-1">
+        Publicado: {{ $post->created_at->format('d/m/Y') }}
+        </span>
+    </div>
+    <h1 class="text-5xl mb-8 mt-2">{{ $post->title }}</h1>
+    <p class="leading-loose text-lg text-gray-700">{{ $post->body }}</p>
+</div>
 
-    <p><strong>Creado Por: </strong><em>{{ $post->user->name }}</em></p>
 @endsection
